b4da1a24db4ac8e023b4b1884b5ce20407b55183
yang0180 <yang0180@algonquinlive.com>
2021-03-29 12:22:09 -0400

    add sqlite open helper

---
+++ app/src/main/java/com/finalproject_cst2335/soccergames/Utils/SoccerGameDBHelper.java
@@ -0,0 +1,42 @@
+package com.finalproject_cst2335.soccergames.Utils;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SoccerGameDBHelper extends SQLiteOpenHelper {
+
+    public static final String TABLE_NAME = "SoccerGames";
+    private static final int DEFAULT_VERSION = 1;
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_TITLE = "title";
+    public static final String COLUMN_DATE = "publish_date";
+    public static final String COLUMN_LINK = "link";
+    public static final String COLUMN_DESCRIPTION = "description";
+    public static final String COLUMN_THUMBNAIL = "thumbnail";
+
+    private static final String DROP_SQL = "DROP TABLE IF EXIST "+TABLE_NAME;
+    private static final String CREATE_TABLE_SQL = " CREATE TABLE IF NOT EXIST "+TABLE_NAME+" ( "
+                                                    + COLUMN_ID + " INTEGER PRIMARY KEY, "
+                                                    + COLUMN_TITLE + " TEXT NOT NULL, "
+                                                    + COLUMN_DATE + " TEXT NOT NULL, "
+                                                    + COLUMN_LINK + " TEXT NOT NULL, "
+                                                    + COLUMN_DESCRIPTION + " TEXT NOT NULL, "
+                                                    + COLUMN_THUMBNAIL + " TEXT NOT NULL )";
+
+    public SoccerGameDBHelper( Context ctx){
+        super(ctx,null,null,DEFAULT_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DROP_SQL);
+        db.execSQL(CREATE_TABLE_SQL);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(DROP_SQL);
+        db.execSQL(CREATE_TABLE_SQL);
+    }
+}
