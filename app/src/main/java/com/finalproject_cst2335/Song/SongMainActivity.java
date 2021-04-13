723c895ce26f21629309d998e0605917042c5e35
HelloNYAC <77427085+HelloNYAC@users.noreply.github.com>
2021-04-13 00:27:17 -0400

    Revised toolbar

--- app/src/main/java/com/finalproject_cst2335/Song/SongMainActivity.java
+++ app/src/main/java/com/finalproject_cst2335/Song/SongMainActivity.java
@@ -1,17 +1,30 @@
 package com.finalproject_cst2335.Song;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.finalproject_cst2335.HomePage;
 import com.finalproject_cst2335.R;
+import com.finalproject_cst2335.car.CarMainActivity;
+import com.finalproject_cst2335.soccergames.SoccerMainActivity;
+import com.finalproject_cst2335.trivia.TriviaMainActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
 /**
@@ -39,6 +52,9 @@ public class SongMainActivity extends AppCompatActivity {
         String ss_nameVal = ss_pref.getString("name_searched","");
         ss_search.setText(ss_nameVal);
 
+        Toolbar tg_myToolbar = (Toolbar)findViewById(R.id.song_my_toolbar_m);
+        setSupportActionBar(tg_myToolbar);
+
         Toast.makeText(this,"Welcome to Songster",Toast.LENGTH_LONG).show();
         ss_gobutton.setOnClickListener(v -> {
             String nameSearched = ss_search.getText().toString();
@@ -62,6 +78,52 @@ public class SongMainActivity extends AppCompatActivity {
         ss_editor.putString("name_searched",ss_nameSearched);
         ss_editor.commit();
 
+    }
+
+    /**
+     * the menu inflater
+     * @param menu
+     * @return
+     */
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.song_main_toolbar_item, menu);
+        return true;
+    }
+
+    /**
+     * opoption selected on toolbar
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        String message = null;
+        //Look at your menu XML file. Put a case for every id in that file:
+        switch(item.getItemId())
+        {
+            case R.id.hp_homepage:
+                message = getResources().getString(R.string.tg_backtomainpage);
+                startActivity(new Intent(this, HomePage.class));
+                break;
+            case R.id.hp_trivia:
+                message = getResources().getString(R.string.zz_gototrivia);
+                startActivity(new Intent(this, TriviaMainActivity.class));
+                break;
+            case R.id.hp_soccer:
+                message = getResources().getString(R.string.zz_gotosoccer);
+                startActivity(new Intent(this, SoccerMainActivity.class));
+                break;
+            case R.id.hp_car:
+                message = getResources().getString(R.string.zz_gotocar);
+                startActivity(new Intent(this, CarMainActivity.class));
+                break;
+            //what to do when the menu item is selected:
+
+        }
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        return true;
     }
 
 
